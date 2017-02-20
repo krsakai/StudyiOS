7c0c6846b97cd42e7bced9d7732867d2f5fb5dc5
sakai kuniya <k_sakai@waku-2.com>
2017-02-20 13:37:49 +0900

    Initialization

--- SampleApp/ViewController.m
+++ SampleApp/ViewController.m
@@ -22,30 +22,4 @@
     [super didReceiveMemoryWarning];
 }
 
-// スイッチの値が変更された時
-- (IBAction)switchValueChanged:(UISwitch *)sender {
-    self.sampleLabel.text = sender.isOn ? @"スイッチはONです" : @"スイッチはOFFです";
-}
-
-// スライダーの値が変更された時
-- (IBAction)sliderValueChanged:(UISlider *)sender {
-    self.sampleLabel.text = [NSString stringWithFormat:@"スライダーは%d%%です", (int)(sender.value * 100)];
-}
-
-// セグメントコントロールの値が変更された時
-- (IBAction)segmentControlValueChanged:(UISegmentedControl *)sender {
-    
-    if (sender.selectedSegmentIndex == 0) {
-        // 1番左のセグメントを選んだ時
-        self.sampleLabel.text = @"セグメントコントロールの1番目がタップされました";
-    } else {
-        // それ以外
-        self.sampleLabel.text = @"セグメントコントロールの2番目がタップされました";
-    }
-}
-
-// ボタンがタップされた時
-- (IBAction)buttonTapped:(id)sender {
-    self.sampleLabel.text = @"各部品を触ってね";
-}
 @end
